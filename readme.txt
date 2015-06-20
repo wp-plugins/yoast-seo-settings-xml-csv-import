34ee8dcba40a80f6829c8d88ec61acdbd3abee70
WPAllImport <WPAllImport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-20 06:07:14 +0000

    First release on WP.org
    git-svn-id: https://plugins.svn.wordpress.org/yoast-seo-settings-xml-csv-import/trunk@1184597 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== Import SEO Settings into WordPress SEO by Yoast ===
+Contributors: soflyy, wpallimport
+Tags: seo, import seo, search engine optimization, import search engine optimization, yoast, import yoast, yoast seo, yoast wordpress seo, import yoast wordpress seo
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Requires at least: 4.1.0
+Tested up to: 4.2.2
+Stable tag: 1.0.0
+
+Easily import SEO settings from any XML or CSV file to Yoast WordPress SEO with the Yoast WordPress SEO Add-On for WP All Import.
+
+== Description ==
+
+The Yoast WordPress SEO Add-On for [WP All Import](http://www.wpallimport.com/order-now/?utm_source=free-plugin&utm_medium=dot-org&utm_campaign=yoast-seo) makes it easy to bulk import your SEO setting to the Yoast WordPress SEO plugin in less than 10 minutes.
+
+The left side of the plugin looks just like Yoast WordPress SEO, and the right side displays the data from your XML/CSV file. Then you can simply drag & drop the data from your XML or CSV into the Yoast WordPress SEO fields to import them.
+
+The importer is so intuitive it is almost like manually configuring Yoast WordPress SEO when adding/editing a post.
+
+= Why you should use the Yoast WordPress SEO Add-On for WP All Import =
+
+* Import new posts, pages, or custom post types and configure their SEO settings all in the same import.
+
+* Update SEO settings for existing posts, pages, or custom post types already published on your site.
+
+* Compatible with all other WP All Import add-ons.
+
+* Complete support - configure all available SEO settings for your posts, including social media images.
+
+= WP All Import Professional Edition =
+
+The Yoast WordPress SEO Add-On for WP All Import is fully compatible with [the free version of WP All Import](http://wordpress.org/plugins/wp-all-import "WordPress XML & CSV Import"). 
+
+However, [the professional edition of WP All Import](http://www.wpallimport.com/order-now/?utm_source=free-plugin&utm_medium=dot-org&utm_campaign=yoast-seo) includes premium support and adds the following features:
+
+* Import files from a URL: Download and import files from external websites, even if they are password protected with HTTP authentication. 
+
+* Cron Job/Recurring Imports: WP All Import Pro can check periodically check a file for updates, and add, edit, delete, and update the your property listings.
+
+* Custom PHP Functions: Pass your data through custom functions by using [my_function({data[1]})] in your import template. WP All Import will pass the value of {data[1]} through my_function and use whatever it returns.
+
+* Access to premium technical support.
+
+[Upgrade to the professional edition of WP All Import now.](http://www.wpallimport.com/order-now/?utm_source=free-plugin&utm_medium=dot-org&utm_campaign=yoast-seo)
+
+== Installation ==
+
+First, install [WP All Import](http://wordpress.org/plugins/wp-all-import "WordPress XML & CSV Import").
+
+Then install the Yoast WordPress SEO Add-On.
+
+To install the Yoast WordPress SEO Add-On, either:
+
+* Upload the plugin from the Plugins page in WordPress
+
+* Unzip import-property-listings-into-realia.zip and upload the contents to /wp-content/plugins/, and then activate the plugin from the Plugins page in WordPress
+
+The Yoast WordPress SEO Add-On will appear in the Step 3 of WP All Import.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release on WP.org.
+
+== Support ==
+
+We do not handle support in the WordPress.org community forums.
+
+We do try to handle support for our free version users at the following e-mail address:
+
+E-mail: support@wpallimport.com
+
+Support for free version customers is not guaranteed and based on ability. For premium support, purchase [WP All Import Pro](http://www.wpallimport.com/order-now/?utm_source=free-plugin&utm_medium=dot-org&utm_campaign=yoast-seo).
---
+++ yoast-addon.php
@@ -0,0 +1,169 @@
+<?php
+
+/*
+Plugin Name: WP All Import - Yoast WordPress SEO Add-On
+Plugin URI: http://www.wpallimport.com/
+Description: Import data into Yoast WordPress SEO with WP All Import.
+Version: 1.0.1
+Author: Soflyy
+*/
+
+include "rapid-addon.php";
+
+$yoast_addon = new RapidAddon( 'Yoast WordPress SEO Add-On', 'yoast_addon' );
+
+$yoast_addon->add_field( '_yoast_wpseo_focuskw', 'Focus Keyword', 'text', null, 'Pick the main keyword or keyphrase that this post/page is about.' );
+
+$yoast_addon->add_field( '_yoast_wpseo_title', 'SEO Title', 'text', null, 'The SEO title defaults to what is generated based on this sites title template for this posttype.' );
+
+$yoast_addon->add_field( '_yoast_wpseo_metadesc', 'Meta Description', 'text', null, 'The meta description will be limited to 156 chars. It is often shown as the black text under the title in a search result. For this to work it has to contain the keyword that was searched for.' );
+
+$yoast_addon->add_options(
+	$yoast_addon->add_field( '_yoast_wpseo_opengraph-title', 'Facebook Title', 'text', null, "If you don't want to use the post title for sharing the post on Facebook but instead want another title there, import it here." ),
+	'Facebook Options',
+	array(
+		$yoast_addon->add_field( '_yoast_wpseo_opengraph-description', 'Description', 'text', null, "If you don't want to use the meta description for sharing the post on Facebook but want another description there, write it here." ),
+		$yoast_addon->add_field( '_yoast_wpseo_opengraph-image', 'Image', 'image', null, "If you want to override the image used on Facebook for this post, import one here. The recommended image size for Facebook is 1200 x 628px."),
+	)
+);
+
+$yoast_addon->add_options(
+	$yoast_addon->add_field( '_yoast_wpseo_twitter-title', 'Twitter Title', 'text', null, "If you don't want to use the post title for sharing the post on Twitter but instead want another title there, import it here." ),
+	'Twitter Options',
+	array(
+		$yoast_addon->add_field( '_yoast_wpseo_twitter-description', 'Description', 'text', null, "If you don't want to use the meta description for sharing the post on Twitter but want another description there, import it here." ),
+		$yoast_addon->add_field( '_yoast_wpseo_twitter-image', 'Image', 'image', null, "If you want to override the image used on Twitter for this post, import one here. The recommended image size for Twitter is 1024 x 512px."),
+	)
+);
+
+$yoast_addon->add_options(
+	null,
+	'Advanced SEO Options',
+	array(
+		$yoast_addon->add_field( '_yoast_wpseo_meta-robots-noindex', 'Meta Robots Index', 'radio', 
+			array(
+				'' => 'default',
+				'2' => 'index',
+				'1' => 'noindex',
+			),
+			"This setting can be overwritten by Yoast WordPress SEO's sitewide privacy settings"
+		),
+		$yoast_addon->add_field( '_yoast_wpseo_meta-robots-nofollow', 'Meta Robots Nofollow', 'radio', 
+			array(
+				'' => 'Follow',
+				'1' => 'Nofollow'
+			) ),
+		$yoast_addon->add_field( '_yoast_wpseo_meta-robots-adv', 'Meta Robots Advanced', 'radio', 
+			array(
+				'' => 'default',
+				'none' => 'None',
+				'noodp' => 'NO ODP',
+				'noydir' => 'NO YDIR',
+				'noimageindex' => 'No Image Index',
+				'noarchive' => 'No Archive',
+				'nosnippet' => 'No Snippet'
+			),
+			'Advanced meta robots settings for this page.'
+		),
+		$yoast_addon->add_field( '_yoast_wpseo_sitemap-include', 'Include in Sitemap', 'radio', 
+			array(
+				'' => 'Auto detect',
+				'always' => 'Always include',
+				'never' => 'Never include'
+			),
+			'Should this page be in the XML Sitemap at all times, regardless of Robots Meta settings?'
+		),
+		$yoast_addon->add_field( '_yoast_wpseo_sitemap-prio', 'Sitemap Priority', 'radio',
+			array(
+				'' => 'Automatic Prioritization',
+				'1' => '1 - Highest priority',
+				'0.9' => '0.9',
+				'0.8' => '0.8 - Default for first tier pages',
+				'0.7' => '0.7',
+				'0.6' => '0.6 - Default for second tier pages and posts',
+				'0.5' => '0.5 - Medium priority',
+				'0.4' => '0.4',
+				'0.3' => '0.3',
+				'0.2' => '0.2',
+				'0.1' => '0.1 - Lowest priority'
+			), 
+			'The priority given to this page in the XML sitemap. '
+			),
+		$yoast_addon->add_field( '_yoast_wpseo_canonical', 'Canonical URL', 'text', null, 'The canonical URL that this page should point to, leave empty to default to permalink. Cross domain canonical supported too.' ),
+		$yoast_addon->add_field( '_yoast_wpseo_redirect', '301 Redirect', 'text', null, 'The URL that this page should redirect to.' )
+
+	)
+);
+
+// add twitter settings
+
+$yoast_addon->set_import_function( 'yoast_seo_addon_import' );
+
+$yoast_addon->admin_notice(
+	'The Yoast WordPress SEO Add-On requires WP All Import <a href="http://www.wpallimport.com/" target="_blank">Pro</a> or <a href="http://wordpress.org/plugins/wp-all-import" target="_blank">Free</a>, and the <a href="https://yoast.com/wordpress/plugins/seo/">Yoast WordPress SEO</a> plugin.',
+	array(
+		"plugins" => array( "wordpress-seo/wp-seo.php" )
+) );
+
+$yoast_addon->run(
+	array(
+		"plugins" => array( "wordpress-seo/wp-seo.php" )
+) );
+
+function yoast_seo_addon_import( $post_id, $data, $import_options ) {
+
+	global $yoast_addon;
+
+    // all fields except for slider and image fields
+    $fields = array(
+    	'_yoast_wpseo_focuskw',
+    	'_yoast_wpseo_title',
+    	'_yoast_wpseo_metadesc',
+    	'_yoast_wpseo_meta-robots-noindex',
+    	'_yoast_wpseo_meta-robots-nofollow',
+    	'_yoast_wpseo_meta-robots-adv',
+    	'_yoast_wpseo_sitemap-include',
+    	'_yoast_wpseo_sitemap-prio',
+    	'_yoast_wpseo_canonical',
+    	'_yoast_wpseo_redirect',
+    	'_yoast_wpseo_opengraph-title',
+    	'_yoast_wpseo_opengraph-description',
+    	'_yoast_wpseo_twitter-title',
+    	'_yoast_wpseo_twitter-description'
+
+    );
+    
+    // image fields
+    $image_fields = array(
+ 		'_yoast_wpseo_opengraph-image',
+ 		'_yoast_wpseo_twitter-image'
+    );
+    
+    $fields = array_merge( $fields, $image_fields );
+    
+    // update everything in fields arrays
+    foreach ( $fields as $field ) {
+
+        if ( $yoast_addon->can_update_meta( $field, $import_options ) ) {
+
+            if ( in_array( $field, $image_fields ) ) {
+
+                if ( $yoast_addon->can_update_image( $import_options ) ) {
+
+                    $id = $data[$field]['attachment_id'];
+                    
+                    $url = wp_get_attachment_url( $id );
+
+                    update_post_meta( $post_id, $field, $url );
+
+                }
+
+            } else {
+
+                update_post_meta( $post_id, $field, $data[$field] );
+
+            }
+        }
+    }
+
+}
